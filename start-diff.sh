91a9fda785f3d304f9f14d5b20733c7893b8c48c
coreycao <caosanyang@gmail.com>
2023-04-14 21:49:33 +0800

    Modify auto run script.

--- start-diff.sh
+++ start-diff.sh
@@ -7,6 +7,6 @@ pipenv run python main.py
 git add .
 today=`date +"%Y-%m-%d"`
 git commit -m "auto update $today"
-#git push origin master
+git push origin main
 
-#0 12 * * * nohup sh /Users/corey/development/playground/py_playground/Movie-250-Diff/start.sh >> /Users/corey/development/playground/py_playground/log/Movie-250-Diff.log 2>&1 &
+#0 23 * * * nohup sh /Users/corey/development/playground/py_playground/Movie-250-Diff/start.sh >> /Users/corey/development/playground/py_playground/log/Movie-250-Diff.log 2>&1 &
