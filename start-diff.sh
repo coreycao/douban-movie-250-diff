f663c871b59739c2104c8b7a6a7eb1786291c993
coreycao <caosanyang@gmail.com>
2023-04-17 20:55:52 +0800

    Add comments.

--- start-diff.sh
+++ start-diff.sh
@@ -1,3 +1,7 @@
+# Add crontab task to run this shell, or set GitHub Action to run the python script.
+
+#0 23 * * * nohup sh /Users/corey/development/playground/py_playground/Movie-250-Diff/start.sh >> /Users/corey/development/playground/py_playground/log/Movie-250-Diff.log 2>&1 &
+
 echo -e "\n----------Start time:----------"
 date
 
@@ -8,5 +12,3 @@ git add .
 today=`date +"%Y-%m-%d"`
 git commit -m "auto update $today"
 git push origin main
-
-#0 23 * * * nohup sh /Users/corey/development/playground/py_playground/Movie-250-Diff/start.sh >> /Users/corey/development/playground/py_playground/log/Movie-250-Diff.log 2>&1 &
